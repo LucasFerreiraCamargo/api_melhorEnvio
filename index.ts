3e4dc29cdac989a0e0f839280724d87b91e15758
Lucas <ferreiracamargolucas@gmail.com>
2025-11-22 22:07:45 -0300

    correcoes

--- index.ts
+++ index.ts
@@ -1,45 +1,18 @@
-import express from 'express'
+// server.ts
+import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors'
-import routesMercadorias from './routes/mercadorias'
-import routesFotos from './routes/fotos'
-import routesFeirantes from "./routes/feirantes"
-import routesUsuarios from "./routes/usuarios"
-// import routesMotoboys from "./routes/Motoboys"
-import routesLogin from "./routes/login"
-import routesPedido from "./routes/pedido"
-import routesAdmins from './routes/admins'
-import routesLoginAdmin from './routes/adminLogin'
-import routesDashboard from './routes/dashboard'
-import routesFeiras from './routes/feiras'
-import routesCestas from './routes/cestas'
-import routesCarrinhos from './routes/carrinho'
-import routesCestasRecorrentes from './routes/cestas_recorrentes'
+// import routes from './routes';
 
-const app = express()
-const port = 3001
+dotenv.config();
 
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(cors())
 
-app.use("/feirantes", routesFeirantes)
-app.use("/mercadorias", routesMercadorias)
-app.use("/usuarios", routesUsuarios)
-app.use("/fotos", routesFotos)
-// app.use("/motoboys", routesMotoboys)
-app.use("/login", routesLogin)
-app.use("/pedido", routesPedido)
-app.use("/admins", routesAdmins)
-app.use("/admins/login", routesLoginAdmin)
-app.use("/dashboard", routesDashboard)
-app.use("/feiras", routesFeiras)
-app.use("/cestas", routesCestas)
-app.use("/carrinhos", routesCarrinhos)
-app.use("/cestas-recorrentes", routesCestasRecorrentes)
+// app.use('/api', routes);
 
-app.get('/', (req, res) => {
-  res.send('API: Venda de Hortifrutis')
-})
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`)
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
